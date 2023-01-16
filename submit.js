dfb3ab30d0d499bbe34bc35230bd5e792460ea42
Mthobisi-za <mthobisimtho333@gmail.com>
2023-01-16 06:54:41 +0200

    done

---
+++ submit.js
@@ -0,0 +1,16 @@
+document.querySelector('.sub').addEventListener('click', () => {
+    var name_and_surname = (document.querySelector('.e_name').value);
+    var e_email = document.querySelector('.e_email').value;
+    var company = document.querySelector('.e_com').value;
+    var fromd = (document.querySelector('.fromd').value).replace('-', '/').replace('-', '/').replace('-', '/');
+    var tod = (document.querySelector('.tod').value).replace('-', '/').replace('-', '/').replace('-', '/');
+    var booked_date = (fromd + ' - ' + tod);
+    console.log(name_and_surname, e_email, booked_date);
+    //'/
+    async function sendnewdata() {
+        var res = await axios.get(`https://newapi-v1.herokuapp.com/newupdate/Mthobisi/Ngubane/${e_email}/${company}/${name_and_surname}/Front%end%developer/${booked_date}`)
+        console.log(res, 'done')
+    };
+    sendnewdata();
+    console.log('done')
+})
