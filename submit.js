af9c1e633a63e9eefdb8cea1a63dc351083f01c9
Mthobisi-za <mthobisimtho333@gmail.com>
2023-01-19 16:06:13 +0200

    done

--- submit.js
+++ submit.js
@@ -2,11 +2,11 @@ document.querySelector('.sub').addEventListener('click', () => {
     var name_and_surname = (document.querySelector('.e_name').value);
     var e_email = document.querySelector('.e_email').value;
     var company = document.querySelector('.e_com').value;
-    var fromd = (document.querySelector('.fromd').value).replace('-', '/').replace('-', '/').replace('-', '/');
-    var tod = (document.querySelector('.tod').value).replace('-', '/').replace('-', '/').replace('-', '/');
-    var booked_date = (fromd + ' - ' + tod);
+    var fromd = (document.querySelector('.fromd').value);
+    var tod = (document.querySelector('.tod').value);
+    var booked_date = (fromd + ' = ' + tod);
     console.log(name_and_surname, e_email, booked_date);
-    //'/
+
     async function sendnewdata() {
         var role = 'Front end Developer';
         var res = await axios.get(`https://calender-api-v2.herokuapp.com/newupdate/Mthobisi/Ngubane/${e_email}/${company}/${name_and_surname}/${role}/${booked_date}`)
@@ -14,4 +14,5 @@ document.querySelector('.sub').addEventListener('click', () => {
     };
     sendnewdata();
     console.log('done');
-})
+});
+// https: //calender-api-v2.herokuapp.com/
